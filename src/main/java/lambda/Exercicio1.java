5b168c03c5d3cb253392956849974e109d677223
Ricardo Lopes Correa <sp.ricardo@gmail.com>
2018-04-14 20:02:39 -0300

    initial

---
+++ src/main/java/lambda/Exercicio1.java
@@ -0,0 +1,46 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	lambda();
+		nomesReduzidos();
+	}
+
+	private static void nomesReduzidos() {
+
+
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9 , 0);
+		 
+		//Implementação tradicional
+		for(Integer n: list) {
+		   System.out.print(n);
+		}
+		 
+		//Implementação com expressões lambda e StreamAPI       
+	
+		list.forEach(n -> System.out.println(n));
+
+
+		
+	}
+
+	private static void lambda() {
+
+		List<String> nomes = Arrays.asList("Lucas", "Rodrigo", "Paulo");
+		
+		 for(String nome : nomes) {
+		     System.out.println(nome);
+		 }
+
+		 nomes.forEach(nome -> System.out.println(nome));
+		
+		
+	}
+
+}
---
+++ src/main/java/lambda/Exercicio2.java
@@ -0,0 +1,9 @@
+package lambda;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		//http://blog.caelum.com.br/chega-de-nullpointerexception-trabalhe-com-o-java-util-optional/
+		
+	}
+}
